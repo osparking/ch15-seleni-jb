cf50521edcdcff2614a8c44d0130e5a23ab06111
osparking <jbpark03@gmail.com>
2024-01-01 13:40:21 +0900

    두 유명 사이트 제목시험-크롬

---
+++ src/test/java/space/bum/junit/browse/ChromeSeleniumTest.java
@@ -0,0 +1,36 @@
+package space.bum.junit.browse;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeSeleniumTest {
+  private WebDriver driver;
+  
+  @Test
+  void testManning() {
+    driver.get("https://www.manning.com/");
+    assertThat(driver.getTitle(), is("Manning"));
+  }
+  
+  @Test
+  void testGoogle() {
+    driver.get("https://www.google.com/");
+    assertThat(driver.getTitle(), is("Google"));
+  }
+  
+  @AfterEach
+  void tearDown() {
+    driver.quit();
+  }
+  
+  @BeforeEach
+  void setUp() {
+    driver = new ChromeDriver();
+  }
+}
