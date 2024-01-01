b0a8ae8b8efa30337503908d96ec1ca2e2f3eb39
osparking <jbpark03@gmail.com>
2024-01-01 20:40:46 +0900

    한 시험을 여러 브라우저에서 시험

---
+++ src/test/java/space/bum/junit/browse/MultiBrowserSeleniumTest.java
@@ -0,0 +1,47 @@
+package space.bum.junit.browse;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultiBrowserSeleniumTest {
+  private WebDriver driver;
+
+  private static Collection<WebDriver> getBrowserBrands() {
+    return Arrays.asList(new WebDriver[] { new ChromeDriver(),
+        new FirefoxDriver(), new EdgeDriver() });
+  }
+
+  @ParameterizedTest
+  @MethodSource("getBrowserBrands")
+  void testManning(WebDriver driver) {
+    this.driver = driver;
+    driver.get("https://www.manning.com/");
+    assertThat(driver.getTitle(), is("Manning"));
+  }
+
+  @Test
+  @MethodSource("getBrowserBrands")
+  void testGoogle(WebDriver driver) {
+    this.driver = driver;
+    driver.get("https://www.google.com/");
+    assertThat(driver.getTitle(), is("Google"));
+  }
+
+  @AfterEach
+  void tearDown() {
+    driver.quit();
+  }
+}
