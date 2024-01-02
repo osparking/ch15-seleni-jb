7875cf5192638e170caf298436acdfa72115e6db
osparking <jbpark03@gmail.com>
2024-01-02 19:31:45 +0900

    로그인 클릭 및 성공, 실패 판단

--- src/test/java/space/bum/junit/pages/LoginPage.java
+++ src/test/java/space/bum/junit/pages/LoginPage.java
@@ -1,10 +1,33 @@
 package space.bum.junit.pages;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage {
+import lombok.AllArgsConstructor;
 
-  public LoginPage(WebDriver webDriver) {
-    // TODO Auto-generated constructor stub
+@AllArgsConstructor
+public class LoginPage {
+  private WebDriver webDriver;
+
+  public LoginPage loginWith(String username, String password) {
+    webDriver.findElement(By.id("username")).sendKeys(username);
+    webDriver.findElement(By.id("password")).sendKeys(password);
+    webDriver.findElement(By.cssSelector("#login button")).click();
+
+    return this;
+  }
+
+  public void thenLoginSuccessful() {
+    assertTrue(
+        webDriver.findElement(By.cssSelector("#flash.success")).isDisplayed());
+    assertTrue(webDriver.findElement(By.cssSelector("[href=\"/logout\"]"))
+        .isDisplayed());
+  }
+
+  public void thenLoginUnsuccessful() {
+    assertTrue(webDriver.findElement(By.id("username")).isDisplayed());
+    assertTrue(webDriver.findElement(By.id("password")).isDisplayed());
   }
 }
